27970c89b54e122d8138c7d78a32640ccbc5c3a7
sairam.k <sairam.k@skillworksit.com>
2025-12-04 14:54:24 +0530

    category crud product crud

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,8 @@ const jobRoutes = require('./jobRoutes');
 const jobApplicationRoute = require('./jobApplicationRoute');
 const reelsRoutes = require('./reelsRoutes');
 const followRoutes = require('./followRoutes');
+const categoryRoutes = require('./categoryRoutes');
+const productRoutes = require('./productRoutes');
 
 module.exports = [
   { path: "/auth", router: authRoutes },
@@ -12,4 +14,6 @@ module.exports = [
   { path: "/applications", router: jobApplicationRoute},
   { path: "/reels", router: reelsRoutes },
   { path: "/follow", router: followRoutes},
+  { path: "/category", router: categoryRoutes},
+  { path: "/products", router: productRoutes },
 ];
